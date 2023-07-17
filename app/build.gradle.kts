f7d84c5545ac0c900676a62ef310b7d360d412fd
Igadwa Magomere <83275851+Brian-Magomere-Igadwa@users.noreply.github.com>
2023-07-17 06:06:39 +0300

    Set up the test nav host for navigation ui tests.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -84,4 +84,10 @@ dependencies {
 
     debugImplementation("androidx.compose.ui:ui-test-manifest")
     debugImplementation("androidx.compose.ui:ui-tooling")
+
+    androidTestImplementation(platform("androidx.compose:compose-bom:2023.05.01"))
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
+    androidTestImplementation("androidx.navigation:navigation-testing:2.6.0")
+    androidTestImplementation("androidx.test.espresso:espresso-intents:3.5.1")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
 }
